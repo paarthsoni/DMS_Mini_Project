edb7ee090e271734142a7dd6a19d639cfa51f40c
paarthsoni <paarth2812@gmail.com>
2021-11-24 15:09:38 +0530

    frame done

---
+++ src/App.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.*;
+import javax.swing.*;
+
+interface frame {
+    JFrame f = new JFrame("Employee's Referral Management System");
+}
+
+class login extends JFrame implements frame, ActionListener {
+
+    public login() {
+
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setDefaultLookAndFeelDecorated(true);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
+public class App extends JFrame implements frame {
+
+    public static void main(String[] args) throws Exception {
+        new login();
+    }
+
+}
